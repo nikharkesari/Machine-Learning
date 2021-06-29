0b8c5b46501960f3b218be0c38811cf1a44d5f10
nikharkesari <king.nike26@gmail.com>
2021-06-29 18:53:54 +0530

    iris

--- Deployment in Machine Learning/flask_iris/app.py
+++ Deployment in Machine Learning/flask_iris/app.py
@@ -1,17 +1,19 @@
 # import Flask class from the flask module
 from flask import Flask, request
-from sklearn.externals import joblib
+from joblib import load
 import numpy as np
 
 # Create Flask object to run
 app = Flask(__name__)
 
 # Load the model from the file
-iris_model = joblib.load('model/iris_model.pkl')
+iris_model = load('model/Iris_model.pkl')
+
 
 @app.route('/')
 def home():
     return "Iris Model Deployment!!"
+
 
 @app.route('/predict')
 def predict():
@@ -20,10 +22,11 @@ def predict():
     sepal_width = request.args['sepal_width']
     petal_length = request.args['petal_length']
     petal_width = request.args['petal_width']
-    
+
     print(sepal_length)
 
-    test_inp = np.array([sepal_length, sepal_width, petal_length, petal_width]).reshape(1, 4)
+    test_inp = np.array(
+        [sepal_length, sepal_width, petal_length, petal_width]).reshape(1, 4)
     class_predicted = int(iris_model.predict(test_inp)[0])
     output = "Predicted Iris Class: " + str(class_predicted)
 
